7aeb9b77b38f5122b26683d5b89cf93f417fac31
Mustafa Turhan <odunluzikkim@gmail.com>
2019-03-05 11:44:41 +0300

    update blog post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,25 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
+
+const Date = styled.p`
+  display: block;
+  margin-bottom: ${rhythm(1)};
+  margin-top: ${rhythm(-1 / 2)};
+`
+
+const OldPosts = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -18,32 +34,10 @@ class BlogPostTemplate extends React.Component {
           description={post.frontmatter.description || post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1 / 2),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
+        <Date style={scale(-1 / 5)}>{post.frontmatter.date}</Date>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
+        <hr style={{ marginBottom: rhythm(1) }} />
+        <OldPosts>
           <li>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
@@ -58,7 +52,7 @@ class BlogPostTemplate extends React.Component {
               </Link>
             )}
           </li>
-        </ul>
+        </OldPosts>
       </Layout>
     )
   }
