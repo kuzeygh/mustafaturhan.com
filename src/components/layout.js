92a7156bd39c67e54c44f90fa242542dcb9cf44a
Mustafa Turhan <odunluzikkim@gmail.com>
2019-03-05 17:52:13 +0300

    add logo to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,74 +1,73 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery } from "gatsby"
+import Image from "gatsby-image"
+import styled from "styled-components"
 
 import { rhythm, scale } from "../utils/typography"
 import Bio from "./bio"
 
+const Title = styled.h1`
+  margin-bottom: ${rhythm(1.5)};
+  margin-top: 0;
+  display: flex;
+  align-items: center;
+`
+
+const HeaderLink = styled(Link)`
+  box-shadow: none;
+  text-decoration: none;
+  color: inherit;
+  margin-left: ${rhythm(1 / 2)};
+`
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: ${rhythm(24)};
+  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+`
+
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.4),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `PT Sans, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    const { title, children } = this.props
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      <StaticQuery
+        query={logoQuery}
+        render={data => {
+          return (
+            <Container>
+              <header>
+                <Title style={{ ...scale(1.4) }}>
+                  <Image
+                    fadeIn={false}
+                    fixed={data.logo.childImageSharp.fixed}
+                    alt="Logo"
+                  />
+                  <HeaderLink to={`/`}>{title}</HeaderLink>
+                </Title>
+              </header>
+              <main>{children}</main>
+              <footer>
+                <Bio />
+              </footer>
+            </Container>
+          )
         }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          <Bio />
-        </footer>
-      </div>
+      />
     )
   }
 }
+
+const logoQuery = graphql`
+  query {
+    logo: file(absolutePath: { regex: "/logo.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default Layout
